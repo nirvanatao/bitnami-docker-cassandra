8ec6c4d6e34e4a6bb8e5c22cb7e69f09139395e8
Javier Salmeron <jsalmeron@bitnami.com>
2019-08-30 15:36:43 +0200

    Migrate container to bash

---
+++ 3/debian-9/rootfs/postunpack.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+# Load libraries
+. /libfs.sh
+. /libcassandra.sh
+
+# Load PostgreSQL environment variables
+eval "$(cassandra_env)"
+
+for dir in "$CASSANDRA_INITSCRIPTS_DIR" "$CASSANDRA_TMP_DIR" "$CASSANDRA_HISTORY_DIR" "$CASSANDRA_CONF_DIR" "$CASSANDRA_LOG_DIR" "$CASSANDRA_MOUNTED_CONF_DIR" "$CASSANDRA_VOLUME_DIR"; do
+    ensure_dir_exists "$dir"
+done
+chmod -R g+rwX "$CASSANDRA_INITSCRIPTS_DIR" "$CASSANDRA_MOUNTED_CONF_DIR" "$CASSANDRA_TMP_DIR" "$CASSANDRA_HISTORY_DIR" "$CASSANDRA_CONF_DIR" "$CASSANDRA_LOG_DIR" "$CASSANDRA_VOLUME_DIR"
+
+# Create wrapper for cqlsh
+cat << EOF > "$CASSANDRA_BIN_DIR/cqlsh"
+#!/bin/sh
+exec "$PYTHON_BIN_DIR/python" "$CASSANDRA_BIN_DIR/cqlsh.py" "\$@"
+EOF
+
+chmod +x "$CASSANDRA_BIN_DIR/cqlsh"
+
+# For backwards compatibility with the bitnami/cassandra 
+# chart v3.x.x we create a link named like the old entrypoint
+ln -s /entrypoint.sh /app-entrypoint.sh
